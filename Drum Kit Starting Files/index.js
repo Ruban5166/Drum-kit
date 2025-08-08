22175e98ad92c4842e5619bced6439eb84374ae1
Ruban5166 <49543080+Ruban5166@users.noreply.github.com>
2025-08-08 16:48:09 +0530

    Update index.js

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,59 +1,112 @@
-var btnLength = document.querySelectorAll(".drum").length
 
-for(var i=0;i<btnLength;i++){
-    document.querySelectorAll(".drum")[i].addEventListener("click",drumClick);
-}
+keyPressed()
 
 
-
-function drumClick(){
- var innerLetter = this.innerHTML;
-
- switch (innerLetter) {
-    case "w":
-        var tom1 = new Audio("sounds/tom-1.mp3")
-        tom1.play()
-        break;
+for(var i =0 ;i<document.querySelectorAll(".drum").length;i++){
 
 
-        case "a":
-            var tom2 = new Audio("sounds/tom-2.mp3")
-            tom2.play()
+document.querySelectorAll(".drum")[i].addEventListener("click",function(){
+
+    var letter = this.innerHTML;
+
+    switch (letter) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            
+            break;
+      case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            
             break;
 
+              case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            
+            break;
+              case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            
+            break;
 
-            case "s":
-                var tom3 = new Audio("sounds/tom-3.mp3")
-                tom3.play()
-                break;
+              case "j":
+            var tom1 = new Audio("sounds/crash.mp3");
+            tom1.play();
+            
+            break;
 
+              case "k":
+            var tom1 = new Audio("sounds/kick-bass.mp3");
+            tom1.play();
+            
+            break;
 
-                case "d":
-                    var tom4 = new Audio("sounds/tom-4.mp3")
-                    tom4.play()
-                    break;
-
-                    case "j":
-                        var snare = new Audio("sounds/crash.mp3")
-                        snare.play()
-                        break;
-
-
-                        case "k":
-                            var crash = new Audio("sounds/kick-bass.mp3")
-                            crash.play()
-                            break;
-
-
-                            case "l":
-                                var kick = new Audio("sounds/snare.mp3")
-                                kick.play()
-                                break;
-    default:
-        console.log(innerLetter)
-        break;
- }
+              case "l":
+            var tom1 = new Audio("sounds/snare.mp3");
+            tom1.play();
+            
+            break;
+        default:
+            break;
+    }
+    
+})
 }
 
-// var audio = new Audio("sounds/tom-1.mp3");
-//     audio.play()
+function keyPressed(){
+    document.addEventListener("keypress",function(event){
+        var key = event.innerHTML;
+        console.log(key)
+          switch (key) {
+        case "w":
+            var tom1 = new Audio("sounds/tom-1.mp3");
+            tom1.play();
+            
+            break;
+      case "a":
+            var tom2 = new Audio("sounds/tom-2.mp3");
+            tom2.play();
+            
+            break;
+
+              case "s":
+            var tom3 = new Audio("sounds/tom-3.mp3");
+            tom3.play();
+            
+            break;
+              case "d":
+            var tom4 = new Audio("sounds/tom-4.mp3");
+            tom4.play();
+            
+            break;
+
+              case "j":
+            var tom1 = new Audio("sounds/crash.mp3");
+            tom1.play();
+            
+            break;
+
+              case "k":
+            var tom1 = new Audio("sounds/kick-bass.mp3");
+            tom1.play();
+            
+            break;
+
+              case "l":
+            var tom1 = new Audio("sounds/snare.mp3");
+            tom1.play();
+            
+            break;
+        default:
+            break;
+    }
+
+    })
+}
+
+
+
+
