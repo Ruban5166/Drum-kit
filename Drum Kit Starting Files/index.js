ed5661408d2f44c811ec024ebcaf17cac8b17374
Ruban5166 <49543080+Ruban5166@users.noreply.github.com>
2025-08-09 12:08:55 +0530

    Update index.js

--- Drum Kit Starting Files/index.js
+++ Drum Kit Starting Files/index.js
@@ -1,66 +1,26 @@
 
-keyPressed()
-
-
 for(var i =0 ;i<document.querySelectorAll(".drum").length;i++){
 
 
 document.querySelectorAll(".drum")[i].addEventListener("click",function(){
 
     var letter = this.innerHTML;
-
-    switch (letter) {
-        case "w":
-            var tom1 = new Audio("sounds/tom-1.mp3");
-            tom1.play();
-            
-            break;
-      case "a":
-            var tom2 = new Audio("sounds/tom-2.mp3");
-            tom2.play();
-            
-            break;
-
-              case "s":
-            var tom3 = new Audio("sounds/tom-3.mp3");
-            tom3.play();
-            
-            break;
-              case "d":
-            var tom4 = new Audio("sounds/tom-4.mp3");
-            tom4.play();
-            
-            break;
-
-              case "j":
-            var tom1 = new Audio("sounds/crash.mp3");
-            tom1.play();
-            
-            break;
-
-              case "k":
-            var tom1 = new Audio("sounds/kick-bass.mp3");
-            tom1.play();
-            
-            break;
-
-              case "l":
-            var tom1 = new Audio("sounds/snare.mp3");
-            tom1.play();
-            
-            break;
-        default:
-            break;
-    }
+    
+   makeSound(letter);
     
 })
 }
 
-function keyPressed(){
-    document.addEventListener("keypress",function(event){
-        var key = event.innerHTML;
-        console.log(key)
-          switch (key) {
+
+
+
+document.addEventListener("keypress",function(event){
+  var key = event.key;
+  makeSound(key)
+})
+
+function makeSound(key){
+   switch (key) {
         case "w":
             var tom1 = new Audio("sounds/tom-1.mp3");
             tom1.play();
@@ -104,9 +64,4 @@ function keyPressed(){
             break;
     }
 
-    })
 }
-
-
-
-
